c2864b943b407bc782835512260fe223081163c1
Berkay Cokgor <berkay_cokgoR@windowslive.com>
2020-01-02 01:41:24 +0300

    Create How2decompyle.py

---
+++ How2decompyle.py
@@ -0,0 +1,54 @@
+import string
+restrictions = [
+ 'uudcjkllpuqngqwbujnbhobowpx_kdkp_',
+ 'f_negcqevyxmauuhthijbwhpjbvalnhnm',
+ 'dsafqqwxaqtstghrfbxzp_x_xo_kzqxck',
+ 'mdmqs_tfxbwisprcjutkrsogarmijtcls',
+ 'kvpsbdddqcyuzrgdomvnmlaymnlbegnur',
+ 'oykgmfa_cmroybxsgwktlzfitgagwxawu',
+ 'ewxbxogihhmknjcpbymdxqljvsspnvzfv',
+ 'izjwevjzooutelioqrbggatwkqfcuzwin',
+ 'xtbifb_vzsilvyjmyqsxdkrrqwyyiu_vb',
+ 'watartiplxa_ktzn_ouwzndcrfutffyzd',
+ 'rqzhdgfhdnbpmomakleqfpmxetpwpobgj',
+ 'qggdzxprwisr_vkkipgftuvhsizlc_pbz',
+ 'jerzhlnsegcaqzathfpuufwunakdtceqw',
+ 'lbvlyyrugffgrwo_v_zrqvqszchqrrljq',
+ 'aiwuuhzbszvfpidwwkl_wynlujbsbhfox',
+ 'vmhrizxtiegxdxsqcdoiyxkffloudwtxg',
+ 'tffjnabob_jbf_qiszdsemczghnjysmah',
+ 'zrqkppvynlkelnevngwlkhgaputhoagtt',
+ 'nl_oojyafwoqccbedijmigpedkdzglq_f',
+ 'cksy_skctjlyxktuzchvstunyvcvabomc',
+ 'ppcxleeguvhvhengmvac_bykhzqohjuei',
+ '_clmaicjrrzhwd_fescyaejtbyefxyihy',
+ 'hhopvwsmjtpjiffzatyhjrev_dwnsidyo',
+ 'sjevtrmkkk_zjalxrxfovjsbcxjx_pskp',
+ 'gnynwuuqypddbsylparpcczqimimqmvdl',
+ 'bxitcmhnmanwuhvjxnqeoiimlegrmkjra']
+alphabet=list(string.ascii_lowercase)
+new=[]
+finalx=[]
+def Diffa(a,b):
+        p = list(set(a)-set(b))
+        finalx.append(p)
+        return(p)
+capital = [0, 4, 9, 19, 23, 26]
+for f in range(0,33):
+    for r in restrictions:
+         new.append(r[f])
+         if(len(new)==26):
+                Diffa(alphabet,new)
+                del new[:]
+flag=""
+count=-1
+for f in finalx:
+        count+=1
+        try:
+                if count in capital:
+                        flag+=f[0].upper()
+                else:
+                        flag+=f[0]
+        except:
+                flag+=("_")
+print ('Yeah, you got it !\nBambooFox{' + flag + '}\n')
